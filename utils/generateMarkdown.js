2b6fc6ac822607dc7d55b6680a83d2c8cf2704bd
Marcela Mejia <mejiao24@hotmail.com>
2022-07-17 00:03:05 +1000

    Implement render license section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -27,7 +27,13 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license===null || license===""){
+    return "";
+  } else {
+    return `Please note this application is covered under the ${renderLicenseLink(license)} license.`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown({title, description, installationInstructions, usageInformation, contributionGuidelines, testInstructions, license, github, email}) {
@@ -67,7 +73,7 @@ ${testInstructions}
 
 ## **License**
 
-${renderLicenseLink(license)}
+${renderLicenseSection(license)}
 
 ## **Questions**
 
